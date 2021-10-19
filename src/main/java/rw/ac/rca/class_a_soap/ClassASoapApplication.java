30800229cfa5dd0413070ad1847c98affb080b45
pauline12ish34 <paulineishimwe12@gmail.com>
2021-10-19 20:31:23 +0200

    first commit

---
+++ src/main/java/rw/ac/rca/class_a_soap/ClassASoapApplication.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.class_a_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassASoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassASoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/ac/rca/class_a_soap/ClassASoapApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.ac.rca.class_a_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassASoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
